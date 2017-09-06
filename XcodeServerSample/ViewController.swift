e6209651b76edee5b09c1e2aecdf9e9703ad77a5
cm-kato-jun <kato.jun@classmethod.jp>
2017-09-06 14:34:44 +0900

    Initial Commit

---
+++ XcodeServerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeServerSample
+//
+//  Created by kato.jun on 2017/09/06.
+//  Copyright © 2017年 Classmethod, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
